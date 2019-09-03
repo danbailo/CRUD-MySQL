e8788a9edf6156a77a0013577fc52859bc0f7506
Daniel Bailo <danbailoufms@gmail.com>
2019-09-03 10:22:24 -0400

    rename files

--- test_new.py
+++
@@ -1,9 +0,0 @@
-class MyClass:
-    def __init__(self,name):
-            self.name = name
-            print("never called in this case")
-    def __new__(cls):
-            return name
-
-obj = MyClass()
-print(obj)
